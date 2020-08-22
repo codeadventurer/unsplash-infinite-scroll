b8b06f0b46763f8cea90252db44675fce7e9af06
Anna Dontcova <a.dontcova@gmail.com>
2020-08-22 11:50:44 +0200

    loading logic ready

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 //Unsplash API
@@ -8,6 +11,14 @@ let photosArray = [];
 const count = 10;
 const apiKey = "gATSasyZk4RV2ztESjK83pLOcWGQ7L-4oryYxVN5rHQ";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Check if all images were loaded
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+  }
+}
 
 // Helper function to set attributes on DOM Elements
 
@@ -18,23 +29,22 @@ function setAttributes(element, attributes) {
 }
 
 function displayPhotos(photosArray) {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
   photosArray.forEach((photo) => {
     const item = document.createElement("a");
-    // item.setAttribute("href", photo.links.html);
-    // item.setAttribute("target", "_blank");
     setAttributes(item, {
       href: photo.links.html,
       target: "_blank",
     });
     const img = document.createElement("img");
-    // img.setAttribute("src", photo.urls.regular);
-    // img.setAttribute("alt", photo.alt_description);
-    // img.setAttribute("title", photo.alt_description);
     setAttributes(img, {
       src: photo.urls.regular,
       alt: photo.alt_description,
       title: photo.alt_description,
     });
+    // Event Listener, check when each is finished loading
+    img.addEventListener("load", imageLoaded());
     item.appendChild(img);
     imageContainer.appendChild(item);
   });
@@ -48,4 +58,19 @@ async function getPhotos() {
   } catch (error) {}
 }
 
+//Check to see if scrolling near bottom of the page, load more photos
+
+window.addEventListener("scroll", () => {
+  const windowHeight = window.innerHeight;
+  const ScrollHeight = window.scrollY;
+  const totalHeight = document.body.offsetHeight;
+
+  if (windowHeight + ScrollHeight >= totalHeight - 1000 && ready) {
+    console.log("getPhotos now");
+    ready = false;
+    getPhotos();
+  }
+});
+
+//On load
 getPhotos();
